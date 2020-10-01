68c35dbeff76c8f8db70240d2b00dbaa0f663b90
Topher <topher.wenceslaus@gmail.com>
2020-10-01 14:04:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const { default: cosmosServer } = require("@zeit/cosmosdb-server");
-const fs = require("fs");
 
-cosmosServer({
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.crt"),
-}).listen(3000, () => {
+cosmosServer().listen(3000, () => {
   console.log(`Cosmos DB server running at https://localhost:3000`);
 });
